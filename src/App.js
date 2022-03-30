7d013d5ce36c8443800ce4026c00df66de8cc702
Practice <josephp123@sky.com>
2022-03-30 11:05:13 +0100

    front-end development cont.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import logo from './res/logo.svg';
+// import logo from './res/logo.svg';
 import './css/App.css';
 import Home from './components/HomePage/Home';
 import NavBar from './components/NavBar/NavBar';
---
+++ src/components/About/MeetTheTeam.jsx
@@ -0,0 +1,32 @@
+import "../../css/AboutStyle.css";
+
+const MeetTheTeam = () => {
+    return (
+      <div className="container" maxWidth="md" >
+        <div className="beauty">
+  
+  
+          <>
+            <h1 > Meet The Team</h1>
+            <hr />
+  
+            <br></br>
+  
+            <Joseph />
+            <hr />
+            <Tasnima />
+            <hr />
+            <Aidan />
+            <hr />
+            <Varis />
+            <hr />
+  
+  
+          </>
+        </div>
+      </div >
+    );
+  };
+  
+  export default MeetTheTeam;
+
